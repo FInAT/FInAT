778679f323e8a35ebe1b445ef02cca8ee8eadd7b
David Ham <David.Ham@imperial.ac.uk>
2014-08-14 21:01:04 +0100

    fecking lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -209,22 +206,22 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     'papersize': 'a4paper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
+    # Latex figure (float) alignment
+    #'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'FInAT.tex', u'FInAT Documentation',
-   u'David A. Ham and Robert C. Kirby', 'manual'),
+    ('index', 'FInAT.tex', u'FInAT Documentation',
+     u'David A. Ham and Robert C. Kirby', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -267,9 +264,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'FInAT', u'FInAT Documentation',
-   u'David A. Ham and Robert C. Kirby', 'FInAT', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'FInAT', u'FInAT Documentation',
+     u'David A. Ham and Robert C. Kirby', 'FInAT', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
