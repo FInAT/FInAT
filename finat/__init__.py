23768f75d03ee288583f7b33973f7e3d87485d20
David A Ham <david.ham@imperial.ac.uk>
2014-08-20 12:13:11 +0100

    import all the elementnames

--- finat/__init__.py
+++ finat/__init__.py
@@ -1,6 +1,7 @@
-
-from lagrange import Lagrange
+from lagrange import Lagrange, DiscontinuousLagrange
+from hdiv import RaviartThomas, BrezziDouglasMarini, BrezziDouglasFortinMarini
 from vectorfiniteelement import VectorFiniteElement
 from points import PointSet
 from utils import KernelData
 from derivatives import div, grad, curl
+
