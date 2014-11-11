5c30e300ce3acb4e06c888ce398117aaa6f67963
David Ham <David.Ham@imperial.ac.uk>
2014-11-11 10:07:32 +0000

    demos (not tests yet) for the interpreter and geometry mapper

---
+++ tests/demo_geometry_mapper.py
@@ -0,0 +1,32 @@
+import FIAT
+import finat
+import pymbolic.primitives as p
+import numpy as np
+
+cell = FIAT.reference_element.UFCTriangle()
+
+lagrange = finat.Lagrange(cell, 1)
+
+vector_lagrange = finat.VectorFiniteElement(lagrange, 2)
+
+lattice = vector_lagrange.cell.make_lattice(1)
+
+X = p.Variable("X")
+
+kernel_data = finat.KernelData(vector_lagrange, X, affine=False)
+
+q = FIAT.quadrature.CollapsedQuadratureTriangleRule(cell, 2)
+
+points = finat.indices.PointIndex(finat.PointSet(q.get_points()))
+
+weights = finat.PointSet(q.get_weights())
+
+var = p.Variable("bar")
+
+recipe = lagrange.field_evaluation(var, points,
+                                   kernel_data, finat.grad, pullback=True)
+recipe = vector_lagrange.moment_evaluation(recipe, weights, points,
+                                           kernel_data, pullback=True)
+
+recipe = finat.GeometryMapper(kernel_data)(recipe)
+print recipe
