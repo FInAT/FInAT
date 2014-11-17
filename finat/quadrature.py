ef88a7754c3d7f57fbc82eb8dae0e49954a1678f
David Ham <David.Ham@imperial.ac.uk>
2014-11-17 21:38:24 +0000

    draft stroud quadrature

--- finat/quadrature.py
+++ finat/quadrature.py
@@ -1,7 +1,50 @@
-import numpy
-import pymbolic.primitives as p
+import numpy as np
+from gauss_jacobi import gauss_jacobi_rule
+from points import StroudPointSet, PointSet
 
 
 class QuadratureRule(object):
-    """Object representing 
-    
+    """Object representing a quadrature rule as a set of points
+    and a set of weights.
+
+    :param cell: The :class:`~FIAT.reference_element.ReferenceElement`
+    on which this quadrature rule is defined.
+    :param points: An instance of a subclass of :class:`points.PointSetBase`
+    giving the points for this quadrature rule.
+    :param weights: The quadrature weights. If ``points`` is a
+    :class:`points.TensorPointSet` then weights is an iterable whose
+    members are the sets of weights along the respective dimensions.
+    """
+
+    def __init__(self, cell, points, weights):
+        self.cell = cell
+        self.points = points
+        self.weights = weights
+
+
+class StroudQuadrature(QuadratureRule):
+    def __init__(self, cell, degree):
+        """Stroud quadrature rule on simplices."""
+
+        sd = cell.get_spatial_dimension()
+
+        if sd != len(cell.vertices) + 1:
+            raise ValueError("cell must be a simplex")
+
+        points = np.zeros((sd, degree))
+        weights = np.zeros((sd, degree))
+
+        for d in range(1, sd+1):
+            [x, w] = gauss_jacobi_rule(sd - d, 0, degree)
+            points[d-1, :] = 0.5 * (x + 1)
+            weights[d-1, :] = w
+
+        scale = 0.5
+        for d in range(1, sd+1):
+            weights[sd-d, :] *= scale
+            scale *= 0.5
+
+        super(StroudQuadrature, self).__init__(
+            cell,
+            StroudPointSet(map(PointSet, points)),
+            weights)
