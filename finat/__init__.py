faa549f8ac99e04bac52dddd0188c079f4cff6c4
David A Ham <david.ham@imperial.ac.uk>
2015-02-16 12:03:58 +0000

    get the name fix right this time

--- finat/__init__.py
+++ finat/__init__.py
@@ -1,9 +1,8 @@
 from lagrange import Lagrange, DiscontinuousLagrange
 from hdiv import RaviartThomas, BrezziDouglasMarini, BrezziDouglasFortinMarini
-from product_elements import ScalarProductElement
 from bernstein import Bernstein
 from vectorfiniteelement import VectorFiniteElement
-from quads import QuadrilateralElement
+from product_elements import ScalarProductElement
 from points import PointSet
 from utils import KernelData
 from derivatives import div, grad, curl
