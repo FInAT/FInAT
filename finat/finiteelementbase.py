c2d7c45b29672ad1dac28ec8648ab685adb2656a
David Ham <David.Ham@imperial.ac.uk>
2016-04-08 23:35:41 +0100

    Draft basis evaluation

--- finat/finiteelementbase.py
+++ finat/finiteelementbase.py
@@ -1,4 +1,5 @@
 import numpy as np
+import gem
 
 
 class UndefinedError(Exception):
@@ -13,14 +14,14 @@ class FiniteElementBase(object):
 
     @property
     def cell(self):
-        '''Return the reference cell on which we are defined.
+        '''The reference cell on which the element is defined.
         '''
 
         return self._cell
 
     @property
     def degree(self):
-        '''Return the degree of the embedding polynomial space.
+        '''The degree of the embedding polynomial space.
 
         In the tensor case this is a tuple.
         '''
@@ -29,7 +30,7 @@ class FiniteElementBase(object):
 
     @property
     def entity_dofs(self):
-        '''Return the map of topological entities to degrees of
+        '''The map of topological entities to degrees of
         freedom for the finite element.
 
         Note that entity numbering needs to take into account the tensor case.
@@ -39,21 +40,33 @@ class FiniteElementBase(object):
 
     @property
     def entity_closure_dofs(self):
-        '''Return the map of topological entities to degrees of
+        '''The map of topological entities to degrees of
         freedom on the closure of those entities for the finite element.'''
 
         raise NotImplementedError
 
     @property
-    def dofs_shape(self):
-        '''Return a tuple indicating the number of degrees of freedom in the
+    def index_shape(self):
+        '''A tuple indicating the number of degrees of freedom in the
         element. For example a scalar quadratic Lagrange element on a triangle
         would return (6,) while a vector valued version of the same element
         would return (6, 2)'''
 
         raise NotImplementedError
 
-    def basis_evaluation(self, index, q, q_index, entity=None, derivative=None):
+    @property
+    def value_shape(self):
+        '''A tuple indicating the shape of the element.'''
+
+        raise NotImplementedError
+
+    def get_indices():
+        '''A tuple of GEM :class:`Index` of the correct extents to loop over
+        the basis functions of this element.'''
+
+        raise NotImplementedError
+
+    def basis_evaluation(self, q, entity=None, derivative=None):
         '''Return code for evaluating the element at known points on the
         reference element.
 
@@ -95,9 +108,44 @@ class FiatElementBase(FiniteElementBase):
         self._cell = cell
         self._degree = degree
 
+    def get_indices():
+        '''A tuple of GEM :class:`Index` of the correct extents to loop over
+        the basis functions of this element.'''
+
+        return (gem.Index(self_fiat_element.get_spatial_dimension()),)
+
+    def basis_evaluation(self, q, entity=None, derivative=0):
+        '''Return code for evaluating the element at known points on the
+        reference element.
+
+        :param q: the quadrature rule.
+        :param entity: the cell entity on which to tabulate.
+        :param derivative: the derivative to take of the basis functions.
+        '''
+
+        assert entity == None
+
+        dim = self.cell.get_spatial_dimension()
+
+        i = self.get_indices()
+        qi = q.get_indices()
+        di = tuple(gem.Index() for i in range(dim)) 
+
+        fiat_tab = self._fiat_element.tabulate(derivative, q.points)
+
+        def tabtensor(pre_indices=()):
+            if len(pre_indices) < dim:
+                return gem.ListTensor([tabtensor(pre_indices + (i,))
+                                       for i in range(derivative + 1)])
+            else:
+                return gem.ListTensor([gem.Literal(fiat_tab.get(pre_indices + (i,)).T, None)
+                                       for i in range(derivative + 1)])
+
+        return ComponentTensor(Indexed(tabtensor(), di + qi + i), qi + i + di)
+
     @property
     def entity_dofs(self):
-        '''Return the map of topological entities to degrees of
+        '''The map of topological entities to degrees of
         freedom for the finite element.
 
         Note that entity numbering needs to take into account the tensor case.
@@ -107,14 +155,14 @@ class FiatElementBase(FiniteElementBase):
 
     @property
     def entity_closure_dofs(self):
-        '''Return the map of topological entities to degrees of
+        '''The map of topological entities to degrees of
         freedom on the closure of those entities for the finite element.'''
 
         return self._fiat_element.entity_closure_dofs()
 
     @property
     def facet_support_dofs(self):
-        '''Return the map of facet id to the degrees of freedom for which the
+        '''The map of facet id to the degrees of freedom for which the
         corresponding basis functions take non-zero values.'''
 
         return self._fiat_element.entity_support_dofs()
