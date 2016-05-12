42e6f9c8ac224c8ed1894a2cac788ff2f670aefb
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-05-12 14:27:26 +0100

    export RT/BDM/BDFM elements

--- finat/__init__.py
+++ finat/__init__.py
@@ -1,4 +1,5 @@
 from fiat_elements import Lagrange, DiscontinuousLagrange, GaussLobatto
+from fiat_elements import RaviartThomas, BrezziDouglasMarini, BrezziDouglasFortinMarini
 from bernstein import Bernstein
 from vectorfiniteelement import VectorFiniteElement
 from product_elements import ScalarProductElement
