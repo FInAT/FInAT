e76f630583cd783f2c1a06ae56c794d258e99161
Miklós Homolya <m.homolya14@imperial.ac.uk>
2016-12-06 16:21:27 +0000

    fix latest flake8 issue

--- finat/derivatives.py
+++ finat/derivatives.py
@@ -11,6 +11,7 @@ class Derivative(object):
     def __str__(self):
         return self.name
 
+
 grad = Derivative("grad", "Symbol for the gradient operation")
 div = Derivative("div", "Symbol for the divergence operation")
 curl = Derivative("curl", "Symbol for the curl operation")
