b38e48297f063743b7139c12125582d147c130e1
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-04-25 17:22:49 +0100

    add FiatElementBase.point_evaluation

--- finat/fiat_elements.py
+++ finat/fiat_elements.py
@@ -1,10 +1,18 @@
 from __future__ import absolute_import, print_function, division
 from six import iteritems
 
-from .finiteelementbase import FiniteElementBase
-import FIAT
-import gem
+from functools import reduce
+
 import numpy as np
+import sympy as sp
+from singledispatch import singledispatch
+
+import FIAT
+from FIAT.polynomial_set import mis, form_matrix_product
+
+import gem
+
+from finat.finiteelementbase import FiniteElementBase
 
 
 class FiatElementBase(FiniteElementBase):
@@ -95,6 +103,66 @@ class FiatElementBase(FiniteElementBase):
                 result[alpha] = expr
         return result
 
+    def point_evaluation(self, order, refcoords, entity=None):
+        '''Return code for evaluating the element at an arbitrary points on
+        the reference element.
+
+        :param order: return derivatives up to this order.
+        :param refcoords: GEM expression representing the coordinates
+                          on the reference entity.  Its shape must be
+                          a vector with the correct dimension, its
+                          free indices are arbitrary.
+        :param entity: the cell entity on which to tabulate.
+        '''
+        assert isinstance(self._element, FIAT.CiarletElement)
+        cell = self.cell
+
+        if entity is None:
+            entity = (cell.get_dimension(), 0)
+        entity_dim, entity_i = entity
+
+        # Spatial dimension of the entity
+        esd = cell.construct_subelement(entity_dim).get_spatial_dimension()
+        assert isinstance(refcoords, gem.Node) and refcoords.shape == (esd,)
+
+        # Coordinates on the reference entity (SymPy)
+        Xi = sp.symbols('X Y Z')[:esd]
+        # Coordinates on the reference cell
+        X = cell.get_entity_transform(entity_dim, entity_i)(Xi)
+
+        # Evaluate expansion set at SymPy point
+        poly_set = self._element.get_nodal_basis()
+        degree = poly_set.get_embedded_degree()
+        base_values = poly_set.get_expansion_set().tabulate(degree, [X])
+        m = len(base_values)
+        assert base_values.shape == (m, 1)
+
+        # Convert SymPy expression to GEM
+        mapper = gem.node.Memoizer(sympy2gem)
+        mapper.bindings = {s: gem.Indexed(refcoords, (i,))
+                           for i, s in enumerate(X)}
+        base_values = gem.ListTensor(list(map(mapper, base_values.flat)))
+
+        # Populate result dict, creating precomputed coefficient
+        # matrices for each derivative tuple.
+        result = {}
+        for i in range(order + 1):
+            for alpha in mis(cell.get_spatial_dimension(), i):
+                D = form_matrix_product(poly_set.get_dmats(), alpha)
+                table = np.dot(poly_set.get_coeffs(), np.transpose(D))
+                assert table.shape[-1] == m
+                beta = tuple(gem.Index() for s in table.shape[:-1])
+                k = gem.Index()
+                result[alpha] = gem.ComponentTensor(
+                    gem.IndexSum(
+                        gem.Product(gem.Indexed(gem.Literal(table), beta + (k,)),
+                                    gem.Indexed(base_values, (k,))),
+                        (k,)
+                    ),
+                    beta
+                )
+        return result
+
 
 class Regge(FiatElementBase):  # naturally tensor valued
     def __init__(self, cell, degree):
@@ -161,3 +229,45 @@ class Nedelec(VectorFiatElement):
 class NedelecSecondKind(VectorFiatElement):
     def __init__(self, cell, degree):
         super(NedelecSecondKind, self).__init__(FIAT.NedelecSecondKind(cell, degree))
+
+
+@singledispatch
+def sympy2gem(node, self):
+    raise AssertionError("sympy node expected, got %s" % type(node))
+
+
+@sympy2gem.register(sp.Expr)
+def sympy2gem_expr(node, self):
+    raise NotImplementedError("no handler for sympy node type %s" % type(node))
+
+
+@sympy2gem.register(sp.Add)
+def sympy2gem_add(node, self):
+    return reduce(gem.Sum, map(self, node.args))
+
+
+@sympy2gem.register(sp.Mul)
+def sympy2gem_mul(node, self):
+    return reduce(gem.Product, map(self, node.args))
+
+
+@sympy2gem.register(sp.Pow)
+def sympy2gem_pow(node, self):
+    return gem.Power(*map(self, node.args))
+
+
+@sympy2gem.register(sp.Integer)
+# @sympy2gem.register(int)
+def sympy2gem_integer(node, self):
+    return gem.Literal(int(node))
+
+
+@sympy2gem.register(sp.Float)
+# @sympy2gem.register(float)
+def sympy2gem_float(node, self):
+    return gem.Literal(node)
+
+
+@sympy2gem.register(sp.Symbol)
+def sympy2gem_symbol(node, self):
+    return self.bindings[node]
