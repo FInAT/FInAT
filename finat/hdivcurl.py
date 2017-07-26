cac6a31cf7a02dc35fda921f92a56fa70a64f105
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-07-26 17:25:02 +0100

    add HCurlElement

--- finat/hdivcurl.py
+++ finat/hdivcurl.py
@@ -9,19 +9,13 @@ from finat.finiteelementbase import FiniteElementBase
 from finat.tensor_product import TensorProductElement
 
 
-class HDivElement(FiniteElementBase):
-    def __init__(self, wrapped):
-        assert isinstance(wrapped, TensorProductElement)
-        if any(fe.formdegree is None for fe in wrapped.factors):
-            raise ValueError("Form degree of subelement is None, cannot H(div)!")
+class WrapperElementBase(FiniteElementBase):
+    """Common base class for H(div) and H(curl) element wrappers."""
 
-        formdegree = sum(fe.formdegree for fe in wrapped.factors)
-        if formdegree != wrapped.cell.get_spatial_dimension() - 1:
-            raise ValueError("H(div) requires (n-1)-form element!")
-
-        super(HDivElement, self).__init__()
+    def __init__(self, wrapped, transform):
+        super(WrapperElementBase, self).__init__()
         self.wrapped = wrapped
-        self.transform = select_hdiv_transformer(wrapped)
+        self.transform = transform
 
     @property
     def cell(self):
@@ -30,10 +24,6 @@ class HDivElement(FiniteElementBase):
     @property
     def degree(self):
         return self.wrapped.degree
-
-    @property
-    def formdegree(self):
-        return self.cell.get_spatial_dimension() - 1
 
     def entity_dofs(self):
         return self.wrapped.entity_dofs()
@@ -72,9 +62,53 @@ class HDivElement(FiniteElementBase):
         core_eval = self.wrapped.point_evaluation(order, refcoords, entity)
         return self._transform_evaluation(core_eval)
 
+
+class HDivElement(WrapperElementBase):
+    """H(div) wrapper element for tensor product elements."""
+
+    def __init__(self, wrapped):
+        assert isinstance(wrapped, TensorProductElement)
+        if any(fe.formdegree is None for fe in wrapped.factors):
+            raise ValueError("Form degree of subelement is None, cannot H(div)!")
+
+        formdegree = sum(fe.formdegree for fe in wrapped.factors)
+        if formdegree != wrapped.cell.get_spatial_dimension() - 1:
+            raise ValueError("H(div) requires (n-1)-form element!")
+
+        transform = select_hdiv_transformer(wrapped)
+        super(HDivElement, self).__init__(wrapped, transform)
+
+    @property
+    def formdegree(self):
+        return self.cell.get_spatial_dimension() - 1
+
     @property
     def mapping(self):
         return "contravariant piola"
+
+
+class HCurlElement(WrapperElementBase):
+    """H(curl) wrapper element for tensor product elements."""
+
+    def __init__(self, wrapped):
+        assert isinstance(wrapped, TensorProductElement)
+        if any(fe.formdegree is None for fe in wrapped.factors):
+            raise ValueError("Form degree of subelement is None, cannot H(curl)!")
+
+        formdegree = sum(fe.formdegree for fe in wrapped.factors)
+        if formdegree != 1:
+            raise ValueError("H(curl) requires 1-form element!")
+
+        transform = select_hcurl_transformer(wrapped)
+        super(HCurlElement, self).__init__(wrapped, transform)
+
+    @property
+    def formdegree(self):
+        return 1
+
+    @property
+    def mapping(self):
+        return "covariant piola"
 
 
 def select_hdiv_transformer(element):
@@ -102,3 +136,33 @@ def select_hdiv_transformer(element):
             assert False, "Unexpected original mapping!"
     else:
         assert False, "Unexpected form degree combination!"
+
+
+def select_hcurl_transformer(element):
+    # Assume: something x interval
+    assert len(element.factors) == 2
+    assert element.factors[1].cell.get_shape() == LINE
+
+    dim = element.cell.get_spatial_dimension()
+    ks = tuple(fe.formdegree for fe in element.factors)
+    if element.mapping == "affine":
+        if ks == (1, 0):
+            # Can only be 2D
+            return lambda v: [v, gem.Zero()]
+        elif ks == (0, 1):
+            # Can be any spatial dimension
+            return lambda v: [gem.Zero()] * (dim - 1) + [v]
+        else:
+            assert False
+    elif element.mapping == "covariant piola":
+        # Second factor must be continuous interval
+        return lambda v: [gem.Indexed(v, (0,)),
+                          gem.Indexed(v, (1,)),
+                          gem.Zero()]
+    elif element.mapping == "contravariant piola":
+        # Second factor must be continuous interval
+        return lambda v: [gem.Product(gem.Literal(-1), gem.Indexed(v, (1,))),
+                          gem.Indexed(v, (0,)),
+                          gem.Zero()]
+    else:
+        assert False, "Unexpected original mapping!"
