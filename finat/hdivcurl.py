dff02c4fd3acb87f49223c724c743fb0b09dd767
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-07-27 14:55:11 +0100

    add more comments

--- finat/hdivcurl.py
+++ finat/hdivcurl.py
@@ -12,31 +12,38 @@ from finat.tensor_product import TensorProductElement
 class WrapperElementBase(FiniteElementBase):
     """Common base class for H(div) and H(curl) element wrappers."""
 
-    def __init__(self, wrapped, transform):
+    def __init__(self, wrappee, transform):
         super(WrapperElementBase, self).__init__()
-        self.wrapped = wrapped
+        self.wrappee = wrappee
+        """An appropriate tensor product FInAT element whose basis
+        functions are mapped to produce an H(div) or H(curl)
+        conforming element."""
+
         self.transform = transform
+        """A transformation applied on the scalar/vector values of the
+        wrapped element to produce an H(div) or H(curl) conforming
+        element."""
 
     @property
     def cell(self):
-        return self.wrapped.cell
+        return self.wrappee.cell
 
     @property
     def degree(self):
-        return self.wrapped.degree
+        return self.wrappee.degree
 
     def entity_dofs(self):
-        return self.wrapped.entity_dofs()
+        return self.wrappee.entity_dofs()
 
     def entity_closure_dofs(self):
-        return self.wrapped.entity_closure_dofs()
+        return self.wrappee.entity_closure_dofs()
 
     def space_dimension(self):
-        return self.wrapped.space_dimension()
+        return self.wrappee.space_dimension()
 
     @property
     def index_shape(self):
-        return self.wrapped.index_shape
+        return self.wrappee.index_shape
 
     @property
     def value_shape(self):
@@ -55,28 +62,28 @@ class WrapperElementBase(FiniteElementBase):
                 for alpha, table in iteritems(core_eval)}
 
     def basis_evaluation(self, order, ps, entity=None):
-        core_eval = self.wrapped.basis_evaluation(order, ps, entity)
+        core_eval = self.wrappee.basis_evaluation(order, ps, entity)
         return self._transform_evaluation(core_eval)
 
     def point_evaluation(self, order, refcoords, entity=None):
-        core_eval = self.wrapped.point_evaluation(order, refcoords, entity)
+        core_eval = self.wrappee.point_evaluation(order, refcoords, entity)
         return self._transform_evaluation(core_eval)
 
 
 class HDivElement(WrapperElementBase):
     """H(div) wrapper element for tensor product elements."""
 
-    def __init__(self, wrapped):
-        assert isinstance(wrapped, TensorProductElement)
-        if any(fe.formdegree is None for fe in wrapped.factors):
+    def __init__(self, wrappee):
+        assert isinstance(wrappee, TensorProductElement)
+        if any(fe.formdegree is None for fe in wrappee.factors):
             raise ValueError("Form degree of subelement is None, cannot H(div)!")
 
-        formdegree = sum(fe.formdegree for fe in wrapped.factors)
-        if formdegree != wrapped.cell.get_spatial_dimension() - 1:
+        formdegree = sum(fe.formdegree for fe in wrappee.factors)
+        if formdegree != wrappee.cell.get_spatial_dimension() - 1:
             raise ValueError("H(div) requires (n-1)-form element!")
 
-        transform = select_hdiv_transformer(wrapped)
-        super(HDivElement, self).__init__(wrapped, transform)
+        transform = select_hdiv_transformer(wrappee)
+        super(HDivElement, self).__init__(wrappee, transform)
 
     @property
     def formdegree(self):
@@ -90,17 +97,17 @@ class HDivElement(WrapperElementBase):
 class HCurlElement(WrapperElementBase):
     """H(curl) wrapper element for tensor product elements."""
 
-    def __init__(self, wrapped):
-        assert isinstance(wrapped, TensorProductElement)
-        if any(fe.formdegree is None for fe in wrapped.factors):
+    def __init__(self, wrappee):
+        assert isinstance(wrappee, TensorProductElement)
+        if any(fe.formdegree is None for fe in wrappee.factors):
             raise ValueError("Form degree of subelement is None, cannot H(curl)!")
 
-        formdegree = sum(fe.formdegree for fe in wrapped.factors)
+        formdegree = sum(fe.formdegree for fe in wrappee.factors)
         if formdegree != 1:
             raise ValueError("H(curl) requires 1-form element!")
 
-        transform = select_hcurl_transformer(wrapped)
-        super(HCurlElement, self).__init__(wrapped, transform)
+        transform = select_hcurl_transformer(wrappee)
+        super(HCurlElement, self).__init__(wrappee, transform)
 
     @property
     def formdegree(self):
@@ -116,19 +123,32 @@ def select_hdiv_transformer(element):
     assert len(element.factors) == 2
     assert element.factors[1].cell.get_shape() == LINE
 
+    # Globally consistent edge orientations of the reference
+    # quadrilateral: rightward horizontally, upward vertically.
+    # Their rotation by 90 degrees anticlockwise is interpreted as the
+    # positive direction for normal vectors.
     ks = tuple(fe.formdegree for fe in element.factors)
     if ks == (0, 1):
+        # Make the scalar value the leftward-pointing normal on the
+        # y-aligned edges.
         return lambda v: [gem.Product(gem.Literal(-1), v), gem.Zero()]
     elif ks == (1, 0):
+        # Make the scalar value the upward-pointing normal on the
+        # x-aligned edges.
         return lambda v: [gem.Zero(), v]
     elif ks == (2, 0):
+        # Same for 3D, so z-plane.
         return lambda v: [gem.Zero(), gem.Zero(), v]
     elif ks == (1, 1):
         if element.mapping == "contravariant piola":
+            # Pad the 2-vector normal on the "base" cell into a
+            # 3-vector, maintaining direction.
             return lambda v: [gem.Indexed(v, (0,)),
                               gem.Indexed(v, (1,)),
                               gem.Zero()]
         elif element.mapping == "covariant piola":
+            # Rotate the 2-vector tangential component on the "base"
+            # cell 90 degrees anticlockwise into a 3-vector and pad.
             return lambda v: [gem.Indexed(v, (1,)),
                               gem.Product(gem.Literal(-1), gem.Indexed(v, (0,))),
                               gem.Zero()]
@@ -143,24 +163,31 @@ def select_hcurl_transformer(element):
     assert len(element.factors) == 2
     assert element.factors[1].cell.get_shape() == LINE
 
+    # Globally consistent edge orientations of the reference
+    # quadrilateral: rightward horizontally, upward vertically.
+    # Tangential vectors interpret these as the positive direction.
     dim = element.cell.get_spatial_dimension()
     ks = tuple(fe.formdegree for fe in element.factors)
     if element.mapping == "affine":
         if ks == (1, 0):
-            # Can only be 2D
+            # Can only be 2D.  Make the scalar value the
+            # rightward-pointing tangential on the x-aligned edges.
             return lambda v: [v, gem.Zero()]
         elif ks == (0, 1):
-            # Can be any spatial dimension
+            # Can be any spatial dimension.  Make the scalar value the
+            # upward-pointing tangential.
             return lambda v: [gem.Zero()] * (dim - 1) + [v]
         else:
             assert False
     elif element.mapping == "covariant piola":
-        # Second factor must be continuous interval
+        # Second factor must be continuous interval.  Just padding.
         return lambda v: [gem.Indexed(v, (0,)),
                           gem.Indexed(v, (1,)),
                           gem.Zero()]
     elif element.mapping == "contravariant piola":
-        # Second factor must be continuous interval
+        # Second factor must be continuous interval.  Rotate the
+        # 2-vector tangential component on the "base" cell 90 degrees
+        # clockwise into a 3-vector and pad.
         return lambda v: [gem.Product(gem.Literal(-1), gem.Indexed(v, (1,))),
                           gem.Indexed(v, (0,)),
                           gem.Zero()]
