fe1b2599339140437ad79cd1d46ab0ab3220e3f5
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2018-06-08 18:23:38 +0100

    Perhaps Morley is right?

--- finat/morley.py
+++ finat/morley.py
@@ -17,32 +17,62 @@ class Morley(ScalarFiatElement):
     def basis_evaluation(self, order, ps, entity=None, coordinate_mapping=None):
         assert coordinate_mapping is not None
 
-        # Jacobians at cell midpoints
-        mps = [self.cell.make_points(1, i, 2)[-1] for i in range(3)]
-        Js = [coordinate_mapping.jacobian_at(mp) for mp in mps]
+        # Jacobians at edge midpoints
+        J = coordinate_mapping.jacobian_at([1/3, 1/3])
 
-        # how to get expr for length of local edge i?
-        elens = [coordinate_mapping.edge_length(i) for i in range(3)]
-        relens = [coordinate_mapping.ref_edge_tangent(i) for i in range(3)]
-        
-        d = 2
-        numbf = 6
-        
-        M = numpy.eye(numbf, dtype=object)
-        
-        def n(J):
-            assert J.shape == (d, d)
-            return numpy.array(
-                [[gem.Indexed(J, (i, j)) for j in range(d)]
-                 for i in range(d)])
+        rns = [coordinate_mapping.reference_normal(i) for i in range(3)]
+        pns = [coordinate_mapping.physical_normal(i) for i in range(3)]
 
-        for multiindex in numpy.ndindex(M.shape):
-            M[multiindex] = gem.Literal(M[multiindex])
+        rts = [coordinate_mapping.reference_tangent(i) for i in range(3)]
+        pts = [coordinate_mapping.physical_tangent(i) for i in range(3)]
 
-        B111
-            
-        # Now put the values into M!
+        pel = coordinate_mapping.physical_edge_lengths()
 
+        # B11 = rns[i, 0]*(pns[i, 0]*J[0,0] + pts[i, 0]*J[1, 0]) + rts[i, 0]*(pns[i, 0]*J[0, 1] + pts[i, 0]*J[1,1])
+
+        # B12 = rns[i, 0]*(pns[i, 1]*J[0,0] + pts[i, 1]*J[1, 0]) + rts[i, 1]*(pns[i, 1]*J[0, 1] + pts[i, 1]*J[1,1])
+
+        B11 = [gem.Sum(gem.Product(gem.Indexed(rns[i], (0, )),
+                                   gem.Sum(gem.Product(gem.Indexed(pns[i], (0, )),
+                                                       gem.Indexed(J, (0, 0))),
+                                           gem.Product(gem.Indexed(pts[i], (0, )),
+                                                       gem.Indexed(J, (1, 0))))),
+                       gem.Product(gem.Indexed(rts[i], (0, )),
+                                   gem.Sum(gem.Product(gem.Indexed(pns[i], (0, )),
+                                                       gem.Indexed(J, (0, 1))),
+                                           gem.Product(gem.Indexed(pts[i], (0, )),
+                                                       gem.Indexed(J, (1, 1))))))
+               for i in range(3)]
+
+        B12 = [gem.Sum(gem.Product(gem.Indexed(rns[i], (0, )),
+                                   gem.Sum(gem.Product(gem.Indexed(pns[i], (1, )),
+                                                       gem.Indexed(J, (0, 0))),
+                                           gem.Product(gem.Indexed(pts[i], (1, )),
+                                                       gem.Indexed(J, (1, 0))))),
+                       gem.Product(gem.Indexed(rts[i], (0, )),
+                                   gem.Sum(gem.Product(gem.Indexed(pns[i], (1, )),
+                                                       gem.Indexed(J, (0, 1))),
+                                           gem.Product(gem.Indexed(pts[i], (1, )),
+                                                       gem.Indexed(J, (1, 1))))))
+               for i in range(3)]
+
+        V = numpy.eye(6, dtype=object)
+        for multiindex in numpy.ndindex(V.shape):
+            V[multiindex] = gem.Literal(V[multiindex])
+
+        for i in range(3):
+            V[i + 3, i + 3] = B11[i]
+
+        V[3, 1] = gem.Division(gem.Product(gem.Literal(-1), B12[0]), gem.Indexed(pel, (0, )))
+        V[3, 2] = gem.Division(B12[0], gem.Indexed(pel, (0, )))
+
+        V[4, 0] = gem.Division(gem.Product(gem.Literal(-1), B12[1]), gem.Indexed(pel, (1, )))
+        V[4, 2] = gem.Division(B12[1], gem.Indexed(pel, (1, )))
+
+        V[5, 0] = gem.Division(gem.Product(gem.Literal(-1), B12[2]), gem.Indexed(pel, (2, )))
+        V[5, 1] = gem.Division(B12[2], gem.Indexed(pel, (2, )))
+
+        M = V.T
 
         M = gem.ListTensor(M)
 
