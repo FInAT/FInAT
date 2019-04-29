7666195401e99304ada2a2a750d11990e4131e77
cyruscycheng21 <cyruscycheng21@gmail.com>
2019-04-29 12:21:38 +0100

    flake8 compliant

--- finat/__init__.py
+++ finat/__init__.py
@@ -9,7 +9,6 @@ from .argyris import Argyris            # noqa: F401
 from .bell import Bell                  # noqa: F401
 from .hermite import Hermite            # noqa: F401
 from .morley import Morley              # noqa: F401
-#from .serendipity import Serendipity  # noqa: F401
 from .trace import HDivTrace  # noqa: F401
 from .spectral import GaussLobattoLegendre, GaussLegendre  # noqa: F401
 from .tensorfiniteelement import TensorFiniteElement  # noqa: F401
