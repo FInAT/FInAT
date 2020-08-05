c8d36b67d57c203d205900e25ac9bd544764ba29
Matthew Kan <ting.kan19@imperial.ac.uk>
2020-08-05 12:59:52 +0000

    Add drafts for TensorFiniteElement, TensorProductelement, EnrichedElement. TensorFintiteElement has additional free indices, TensorProduct Element requires multiplication accross duals in dual_basis, EnrichedEleemnt is untested.

--- finat/enriched.py
+++ finat/enriched.py
@@ -128,26 +128,28 @@ class EnrichedElement(FiniteElementBase):
         return self._compose_evaluations(results)
 
     def dual_basis(self):
-        from itertools import chain
+        from itertools import chain, zip_longest
 
-        # Note: NOT TESTED!!!
         elements_dual_basis = [element.dual_basis() for element in self.elements]
-        return tuple(chain(*elements_dual_basis))
 
-        # enriched_dual_basis = []
-        # for elements_dual in zip_longest(*elements_dual_basis, fill_value=tuple()):
-        #     enriched_derivs = []
-        #     for elements_derivs in zip_longest(*elements_dual, fill_value=tuple()):
-        #         enriched_pts_in_derivs = []
-        #         for elements_tups in zip_longest(*elements_derivs, fill_value=tuple()):
-        #             # TODO: Combine repeated points?
-        #             enriched_pts_in_derivs.extend(elements_tups)
-        #         # Leave one empty tup for safety?
-        #         for _ in range(enriched_pts_in_derivs.count(tuple())-2):
-        #             enriched_pts_in_derivs.remove(tuple())
-        #         enriched_derivs.append(tuple(enriched_pts_in_derivs))
-        #     enriched_dual_basis.append(tuple(enriched_derivs))
-        # return tuple(enriched_dual_basis)
+        enriched_dual_basis = []
+        for elements_dual in zip_longest(*elements_dual_basis, fill_value=[]):
+            enriched_derivs = []
+            for elements_deriv in zip_longest(*elements_dual, fill_value=[]):
+                enriched_pts_in_derivs = []
+                for elements_tups in zip_longest(*elements_deriv, fill_value=[]):
+                    # TODO: Combine repeated points?
+                    # TODO: Remove empty tuples?
+                    for tups in elements_tups:
+                        try:
+                            elements_point_set, weight_tensor, alpha_tensor = elements_tups
+                        except ValueError:  # Empty
+                            continue
+
+                    enriched_pts_in_derivs.append(chain(*elements_tups))
+                enriched_derivs.append(tuple(enriched_pts_in_derivs))
+            enriched_dual_basis.append(tuple(enriched_derivs))
+        return tuple(enriched_dual_basis)
 
     @property
     def mapping(self):
