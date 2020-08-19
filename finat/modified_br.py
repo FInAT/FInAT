3634f4a05ee53b1a49aa8c5b3063aaef417bc724
Lawrence Mitchell <lawrence@wence.uk>
2020-08-19 16:28:28 +0100

    WIP: We're going to build the basis directly
    Lots of sympy futzing.

--- finat/modified_br.py
+++ finat/modified_br.py
@@ -1,13 +1,15 @@
+from itertools import combinations, count
+from operator import methodcaller
+
+import gem
 import numpy
+import numpy as np
+import sympy as sp
+from FIAT.polynomial_set import mis
+from FIAT.reference_element import UFCTetrahedron, UFCTriangle, ufc_simplex
 
 from finat.finiteelementbase import FiniteElementBase
-from finat.physically_mapped import DirectlyDefinedElement, Citations
-from FIAT.reference_element import UFCTriangle, UFCTetrahedron
-from FIAT.polynomial_set import mis
-
-from itertools import chain
-import gem
-import sympy
+from finat.physically_mapped import Citations, DirectlyDefinedElement
 from finat.sympy2gem import sympy2gem
 
 
@@ -72,13 +74,13 @@ class ModifiedBR(DirectlyDefinedElement, FiniteElementBase):
         dim = self.cell.get_spatial_dimension()
 
         if dim == 2:
-            phys_cell_vertices = numpy.asarray(sympy.symbols('x:3,y:3')).reshape(2, 3).T
+            phys_cell_vertices = numpy.asarray(sp.symbols('x:3,y:3')).reshape(2, 3).T
         elif dim == 3:
-            phys_cell_vertices = numpy.asarray(sympy.symbols('x:4,y:4,z:4')).reshape(3, 4).T
+            phys_cell_vertices = numpy.asarray(sp.symbols('x:4,y:4,z:4')).reshape(3, 4).T
 
         gem_cell_vertices = coordinate_mapping.physical_vertices()
 
-        phys_quad_points = sympy.symbols('x,y,z')[:dim]
+        phys_quad_points = sp.symbols('x,y,z')[:dim]
         gem_quad_points = gem.partial_indexed(
             coordinate_mapping.physical_points(ps, entity=entity),
             ps.indices)
@@ -115,8 +117,8 @@ def basis_evaluation(cell_vertices, quad_points, cell, degree):
     # First the vertex dofs
     lagrange = lagrange_basis(cell_vertices, quad_points, cell, degree)
     # vector-expand
-    basis = ([sympy.Array([b, 0]) for b in lagrange]
-             + [sympy.Array([0, b]) for b in lagrange])
+    basis = ([sp.Array([b, 0]) for b in lagrange]
+             + [sp.Array([0, b]) for b in lagrange])
     return basis
 
 
@@ -138,6 +140,134 @@ def lagrange_basis(cell_vertices, quad_points, cell, degree):
          [dof2(basis[0]), dof2(basis[1]), dof2(basis[2])],
          [dof3(basis[0]), dof3(basis[1]), dof3(basis[2])]]
 
-    AT = sympy.Matrix(A).T
+    AT = sp.Matrix(A).T
     coefficients = AT.inv('LU')
-    return coefficients * sympy.Matrix(basis)
+    return coefficients * sp.Matrix(basis)
+
+
+def numbering(dimension):
+    if dimension == 2:
+        pass
+    elif dimension == 3:
+        raise NotImplementedError()
+
+    # Counter clockwise for vertices on reference triangle (barycentre
+    # is appended)
+    cell2vertex = {0: [0, 1, 3],
+                   1: [0, 2, 3],
+                   2: [1, 2, 3]}
+
+    # refinement is:
+    # bottom edge, left edge, diagonal edge.
+    # vertex dofs have global numbering first, then edges
+    # local numbering is FIAT entity_dof convention for P2
+    cell2node = {0: [0, 1, 3, 6, 5, 4],
+                 1: [0, 2, 3, 8, 5, 7],
+                 2: [1, 2, 3, 8, 6, 9]}
+
+    return cell2vertex, cell2node
+
+
+def integrate(subcell_vertices, expr, X):
+    from sp.geometry.point import Point
+    from sp.geometry.polygon import Polygon
+    from sp.integrals.intpoly import polytope_integrate, x, y, z
+
+    expr = expr.subs(dict(zip(X, (x, y, z))))
+    polygon = Polygon(*(Point(*vtx) for vtx in subcell_vertices))
+    flipsign = sp.simplify(polytope_integrate(polygon, expr=1)) < 0
+
+    val = sp.simplify(polytope_integrate(polygon, expr))
+    if flipsign:
+        val = val * -1
+    return val.subs(dict(zip((x, y, z), X)))
+
+
+def beta(xy, f):
+    """
+        xy: array of [nvertices, dim] representing the coordinates
+        f : index of face to calculate beta for,
+            the face f is opposite vertex f
+    """
+
+    dim = 2
+    fiat_cell = ufc_simplex(dim)
+    topology = fiat_cell.get_topology()
+    X = sp.symbols(f'x:{dim}')
+    # FIXME: figure out monomial basis for P2 in arbitrary dimensions
+    scalar_basis = [1 + 0*X[0], X[0], X[1], X[0]**2, X[0]*X[1], X[1]**2]
+
+    # We need to make a basis for CG2 on the barycentric
+    # refinement of our triangle.
+
+    bary = xy.sum(axis=0)/(dim+1)
+    meshxy = np.vstack([xy, bary])
+    cell2vertex, cell2node = numbering(dim)
+    CG2_basis = []
+    for cell in range(dim+1):
+        assert cell2vertex[cell] == cell2node[cell][:dim+1]
+        dof_vertices = [methodcaller('subs', dict(zip(X, meshxy[cell2vertex[cell][i], :])))
+                        for i in range(dim+1)]
+
+        dof_edges = []
+        for edge in range(len(list(combinations(range(dim+1), 2)))):
+            # FIXME, use factorial for this
+            v1, v2 = topology[1][edge]
+            gv1 = cell2vertex[cell][v1]
+            gv2 = cell2vertex[cell][v2]
+            coord = 0.5*(meshxy[gv1, :] + meshxy[gv2, :])
+            print(coord)
+            dof = methodcaller('subs', dict(zip(X, coord)))
+            dof_edges.append(dof)
+        print("-"*80)
+
+        dofs = dof_vertices + dof_edges
+
+        A = [[dofi(basisj) for basisj in scalar_basis]
+             for dofi in dofs]
+
+        AT = sp.Matrix(A).T
+        coefficients = sp.simplify(AT.inv('LU'))
+        CG2_basis.append(coefficients * sp.Matrix(scalar_basis))
+
+    m = np.zeros((20, 20), dtype=object)
+    counter = count()
+    # Equations on boundary (lagrange dofs)
+    for node in [0, 1, 2, 4, 7, 9]:
+        for i in range(dim):
+            c = next(counter)
+            m[c, dim*node + i] = 1 + 0*X[0]
+
+
+    # Want div(B) = constant on each subcell
+    # Obtained by orthogonality against the legendre polynomial of
+    # degree 1. On [-1, 1], this is just X. On [0, 1] it's
+
+    # i.e. \int div(B)*x = 0 and \int div(B) * y on each subcell
+    V = [1 + X[0]*0] + [X[i] for i in range(dim)]
+
+    def proj(u, v, xy):
+        return u*integrate(xy, u*v, X) / integrate(xy, u*u, X)
+
+    for cell in range(dim+1):
+        # Do partial Gram-Schmidt to make an L2-orthogonal basis for
+        # P1 on subcell.
+        U = [V[0]]
+        cellxy = meshxy[cell2vertex[cell], ...]
+        # Just need to orthogonalise against 1, because we know X, Y,
+        # and Z are already independent
+        for s in range(dim):
+            U.append(V[s+1] - proj(U[0], V[s+1], cellxy))
+
+        # We now want to integrate div(B) against these polynomials to
+        # get our constraint equations.
+        # On this cell.
+        # one row. column is int(div(basis_function)*U)
+        # RHS is 0
+    return CG2_basis, X
+
+
+xy = np.array([[0, 0],
+               [1, 0],
+               [0, 1]])
+# basis, X = beta(xy, 0)
