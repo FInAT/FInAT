7da85a6cd15b1763d6c30a6e6c1b29d6a6a6a459
Pablo Brubeck <brubeck@protonmail.com>
2023-11-10 22:45:44 +0000

    lint E721 do not compare types, for exact checks use is

--- finat/point_set.py
+++ finat/point_set.py
@@ -197,7 +197,7 @@ class TensorPointSet(AbstractPointSet):
 
     def almost_equal(self, other, tolerance=1e-12):
         """Approximate numerical equality of point sets"""
-        return type(self) == type(other) and \
+        return type(self) is type(other) and \
             len(self.factors) == len(other.factors) and \
             all(s.almost_equal(o, tolerance=tolerance)
                 for s, o in zip(self.factors, other.factors))
