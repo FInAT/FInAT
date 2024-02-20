83ddfb0f209ba25c06bbd34d21ec7072b8c183b9
Pablo Brubeck <brubeck@protonmail.com>
2024-02-20 16:12:27 +0000

    Add GLL quadrature scheme

--- finat/quadrature.py
+++ finat/quadrature.py
@@ -7,10 +7,12 @@ import gem
 from gem.utils import cached_property
 
 from FIAT.reference_element import LINE, QUADRILATERAL, TENSORPRODUCT
-from FIAT.quadrature import GaussLegendreQuadratureLineRule
+from FIAT.quadrature import (GaussLegendreQuadratureLineRule,
+                             GaussLobattoLegendreQuadratureLineRule)
 from FIAT.quadrature_schemes import create_quadrature as fiat_scheme
 
-from finat.point_set import PointSet, GaussLegendrePointSet, TensorPointSet
+from finat.point_set import (GaussLegendrePointSet, GaussLobattoLegendrePointSet,
+                             PointSet, TensorPointSet)
 
 
 def make_quadrature(ref_el, degree, scheme="default"):
@@ -26,6 +28,11 @@ def make_quadrature(ref_el, degree, scheme="default"):
     :arg ref_el: The FIAT cell to create the quadrature for.
     :arg degree: The degree of polynomial that the rule should
         integrate exactly.
+    :arg scheme: The quadrature scheme. Choose from
+        'default' FIAT chooses an optimal rule,
+        'canonical' for the Stroud conical product rule (simplex cells only),
+        'GLL' for spectral collocation mass-lumping (tensor-product cells only),
+        'KMV' for mass-lumping (simplex cells only).
     """
     if ref_el.get_shape() == TENSORPRODUCT:
         try:
@@ -45,13 +52,24 @@ def make_quadrature(ref_el, degree, scheme="default"):
         raise ValueError("Need positive degree, not %d" % degree)
 
     if ref_el.get_shape() == LINE:
-        # FIAT uses Gauss-Legendre line quadature, however, since we
-        # symbolically label it as such, we wish not to risk attaching
-        # the wrong label in case FIAT changes.  So we explicitly ask
-        # for Gauss-Legendre line quadature.
-        num_points = (degree + 1 + 1) // 2  # exact integration
-        fiat_rule = GaussLegendreQuadratureLineRule(ref_el, num_points)
-        point_set = GaussLegendrePointSet(fiat_rule.get_points())
+        num_points = (degree + 1 + 1) // 2  # exact integration (GL only)
+        if scheme.lower() == "gll":
+            # GLL under-integration / Mass-lumping
+            num_points = max(num_points, 2)  # GLL rule only valid for >= 2 points
+            make_fiat_rule = GaussLobattoLegendreQuadratureLineRule
+            make_point_set = GaussLobattoLegendrePointSet
+        elif scheme.lower() in ["gl", "default", "canonical"]:
+            # FIAT uses Gauss-Legendre line quadature, however, since we
+            # symbolically label it as such, we wish not to risk attaching
+            # the wrong label in case FIAT changes.  So we explicitly ask
+            # for Gauss-Legendre line quadature.
+            make_fiat_rule = GaussLegendreQuadratureLineRule
+            make_point_set = GaussLegendrePointSet
+        else:
+            raise ValueError(f"Invalid quadrature scheme {scheme}.")
+
+        fiat_rule = make_fiat_rule(ref_el, num_points)
+        point_set = make_point_set(fiat_rule.get_points())
         return QuadratureRule(point_set, fiat_rule.get_weights())
 
     fiat_rule = fiat_scheme(ref_el, degree, scheme)
