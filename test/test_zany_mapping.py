98835e9cbae98c7dc31911e129be427f4f9b7e8b
Pablo Brubeck <brubeck@protonmail.com>
2024-10-28 10:50:19 +0000

    update tests

--- test/test_zany_mapping.py
+++ test/test_zany_mapping.py
@@ -39,71 +39,6 @@ def make_unisolvent_points(element, interior=False):
 def check_zany_mapping(element, ref_cell, phys_cell, *args, **kwargs):
     phys_element = element(phys_cell, *args, **kwargs).fiat_equivalent
     finat_element = element(ref_cell, *args, **kwargs)
-    ref_element = finat_element.fiat_equivalent
-    shape = ref_element.value_shape()
-
-    ref_cell = ref_element.get_reference_element()
-    phys_cell = phys_element.get_reference_element()
-    mapping = MyMapping(ref_cell, phys_cell)
-
-    ref_points = make_unisolvent_points(ref_element)
-    ps = finat.point_set.PointSet(ref_points)
-
-    z = (0,) * finat_element.cell.get_spatial_dimension()
-    finat_vals_gem = finat_element.basis_evaluation(0, ps, coordinate_mapping=mapping)[z]
-    finat_vals = evaluate([finat_vals_gem])[0].arr.transpose(*range(1, len(shape)+2), 0)
-
-    phys_points = make_unisolvent_points(phys_element)
-    phys_vals = phys_element.tabulate(0, phys_points)[z]
-
-    num_dofs = finat_element.space_dimension()
-    num_bfs = phys_element.space_dimension()
-
-    # Solve for the basis transformation and compare results
-    ref_vals = ref_element.tabulate(0, ref_points)[z]
-    Phi = ref_vals.reshape(num_bfs, -1)
-    phi = phys_vals.reshape(num_bfs, -1)
-    Mh = np.linalg.solve(Phi @ Phi.T, Phi @ phi.T).T
-    Mh = Mh[:num_dofs]
-    Mh[abs(Mh) < 1E-10] = 0
-
-    Mgem = finat_element.basis_transformation(mapping)
-    M = evaluate([Mgem])[0].arr
-    assert np.allclose(M, Mh, atol=1E-9), str(Mh.T-M.T)
-    assert np.allclose(finat_vals, phys_vals[:num_dofs])
-
-
-@pytest.mark.parametrize("element", [
-                         finat.Morley,
-                         finat.QuadraticPowellSabin6,
-                         finat.QuadraticPowellSabin12,
-                         finat.Hermite,
-                         finat.ReducedHsiehCloughTocher,
-                         finat.Bell,
-                         ])
-def test_C1_elements(ref_cell, phys_cell, element):
-    finat_kwargs = {}
-    if element == finat.QuadraticPowellSabin12:
-        finat_kwargs = dict(avg=True)
-    check_zany_mapping(element, ref_cell, phys_cell, **finat_kwargs)
-
-
-@pytest.mark.parametrize("element, degree", [
-    *((finat.Argyris, k) for k in range(5, 8)),
-    *((finat.HsiehCloughTocher, k) for k in range(3, 6))
-])
-def test_high_order_C1_elements(ref_cell, phys_cell, element, degree):
-    check_zany_mapping(element, ref_cell, phys_cell, degree, avg=True)
-
-
-def test_argyris_point(ref_cell, phys_cell):
-    element = finat.Argyris
-    check_zany_mapping(element, ref_cell, phys_cell, variant="point")
-
-
-def check_zany_piola_mapping(element, ref_cell, phys_cell, *args, **kwargs):
-    phys_element = element(phys_cell, *args, **kwargs).fiat_equivalent
-    finat_element = element(ref_cell, *args, **kwargs)
 
     ref_element = finat_element._element
     ref_cell = ref_element.get_reference_element()
@@ -117,29 +52,40 @@ def check_zany_piola_mapping(element, ref_cell, phys_cell, *args, **kwargs):
     phys_pts = make_unisolvent_points(phys_element, interior=True)
     phys_vals = phys_element.tabulate(0, phys_pts)[(0,)*sd]
 
-    # Piola map the reference elements
-    J, b = FIAT.reference_element.make_affine_mapping(ref_cell.vertices,
-                                                      phys_cell.vertices)
-    detJ = np.linalg.det(J)
-    K = J / detJ
-    if len(shape) == 2:
-        piola_map = lambda x: K @ x @ K.T
+    mapping = ref_element.mapping()[0]
+    if mapping == "affine":
+        ref_vals_piola = ref_vals
     else:
-        piola_map = lambda x: K @ x
+        # Piola map the reference elements
+        J, b = FIAT.reference_element.make_affine_mapping(ref_cell.vertices,
+                                                          phys_cell.vertices)
+        K = []
+        if "covariant" in mapping:
+            K.append(np.linalg.inv(J).T)
+        if "contravariant" in mapping:
+            K.append(J / np.linalg.det(J))
 
-    ref_vals_piola = np.zeros(ref_vals.shape)
-    for i in range(ref_vals.shape[0]):
-        for k in range(ref_vals.shape[-1]):
-            ref_vals_piola[i, ..., k] = piola_map(ref_vals[i, ..., k])
+        if len(shape) == 2:
+            piola_map = lambda x: K[0] @ x @ K[-1].T
+        else:
+            piola_map = lambda x: K[0] @ x
 
-    num_bfs = phys_element.space_dimension()
-    num_dofs = finat_element.space_dimension()
+        ref_vals_piola = np.zeros(ref_vals.shape)
+        for i in range(ref_vals.shape[0]):
+            for k in range(ref_vals.shape[-1]):
+                ref_vals_piola[i, ..., k] = piola_map(ref_vals[i, ..., k])
 
     # Zany map the results
+    num_bfs = phys_element.space_dimension()
+    num_dofs = finat_element.space_dimension()
     mappng = MyMapping(ref_cell, phys_cell)
-    Mgem = finat_element.basis_transformation(mappng)
-    M = evaluate([Mgem])[0].arr
-    ref_vals_zany = np.tensordot(M, ref_vals_piola, (-1, 0))
+    try:
+        Mgem = finat_element.basis_transformation(mappng)
+        M = evaluate([Mgem])[0].arr
+        ref_vals_zany = np.tensordot(M, ref_vals_piola, (-1, 0))
+    except AttributeError:
+        M = np.eye(num_dofs, num_bfs)
+        ref_vals_zany = ref_vals_piola
 
     # Solve for the basis transformation and compare results
     Phi = ref_vals_piola.reshape(num_bfs, -1)
@@ -150,6 +96,33 @@ def check_zany_piola_mapping(element, ref_cell, phys_cell, *args, **kwargs):
     M[abs(M) < 1E-10] = 0.0
     assert np.allclose(M, Mh), str(M.T - Mh.T)
     assert np.allclose(ref_vals_zany, phys_vals[:num_dofs])
+
+
+@pytest.mark.parametrize("element", [
+                         finat.Morley,
+                         finat.QuadraticPowellSabin6,
+                         finat.QuadraticPowellSabin12,
+                         finat.Hermite,
+                         finat.ReducedHsiehCloughTocher,
+                         finat.Bell,
+                         ])
+def test_C1_elements(ref_cell, phys_cell, element):
+    kwargs = {}
+    if element == finat.QuadraticPowellSabin12:
+        kwargs = dict(avg=True)
+    check_zany_mapping(element, ref_cell, phys_cell, **kwargs)
+
+
+@pytest.mark.parametrize("element, degree", [
+    *((finat.Argyris, k) for k in range(5, 8)),
+    *((finat.HsiehCloughTocher, k) for k in range(3, 6))
+])
+def test_high_order_C1_elements(ref_cell, phys_cell, element, degree):
+    check_zany_mapping(element, ref_cell, phys_cell, degree, avg=True)
+
+
+def test_argyris_point(ref_cell, phys_cell):
+    check_zany_mapping(finat.Argyris, ref_cell, phys_cell, variant="point")
 
 
 @pytest.mark.parametrize("element", [
@@ -167,14 +140,14 @@ def check_zany_piola_mapping(element, ref_cell, phys_cell, *args, **kwargs):
                          finat.GuzmanNeilanBubble,
                          ])
 def test_piola_triangle(ref_cell, phys_cell, element):
-    check_zany_piola_mapping(element, ref_cell, phys_cell)
+    check_zany_mapping(element, ref_cell, phys_cell)
 
 
 @pytest.mark.parametrize("element, degree, variant", [
     *((finat.HuZhang, k, v) for v in ("integral", "point") for k in range(3, 6)),
 ])
 def test_piola_triangle_high_order(ref_cell, phys_cell, element, degree, variant):
-    check_zany_piola_mapping(element, ref_cell, phys_cell, degree, variant)
+    check_zany_mapping(element, ref_cell, phys_cell, degree, variant)
 
 
 @pytest.fixture
@@ -205,4 +178,4 @@ def phys_tet(request):
                          finat.GuzmanNeilanH1div,
                          ])
 def test_piola_tetrahedron(ref_tet, phys_tet, element):
-    check_zany_piola_mapping(element, ref_tet, phys_tet)
+    check_zany_mapping(element, ref_tet, phys_tet)
